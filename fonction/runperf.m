ccb24da946107f48b5cced58acda4d56e9bea99d
Ulrich930 <ulrichlumendo@gmail.com>
2024-07-06 20:41:55 +0100

    revision

---
+++ fonction/runperf.m
@@ -0,0 +1,16 @@
+function runperf(fct_transfert)
+    % fct_transfert point de départ du projet
+    %% Critère de Routh
+    % Tester la stabilité d'un système via sa fonction de transfer
+    [stable, droite, gauche] = routh(fct_transfert(2,:));
+    
+    if stable == 0
+        disp('Le système est instable selon le critère de Routh');
+        fprintf('Il y a %g poles dans le demi-plan de gauche et %d dans le demi-plan de de droite\n', gauche, droite);
+    else
+        disp('Le sytème est stable selon le critère de Routh')
+        disp('')
+        disp('Nous pouvons passer à la prochaine étape')
+    end
+
+
--- main.m
+++ main.m
@@ -3,7 +3,8 @@
 %%%%   COURS D'AUTOMATIQUE LINEAIRE %%%%
 %%%%   PROJET N°1 GROUPE 1          %%%%
 
-%%  PROF GUY WALONGO, titulaire du cours
-%%  Programme principal du projet
+%%%%  PROF GUY WALONGO, titulaire du cours
+%%%%  Programme principal du projet
 
-
+%% Importer les focntions
+addpath('fonction\')
