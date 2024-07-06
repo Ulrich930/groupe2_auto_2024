cfb3826d690c15ab8f53202bee7a7d59823a8482
Exaucé MARUBA <63616448+Maruba22@users.noreply.github.com>
2024-07-06 22:42:59 +0100

    Valid fin  calculs erreurs

--- fonction/performance.m
+++ fonction/performance.m
@@ -1,7 +1,7 @@
 function[erpeeu, erperu] = performance(fct_transfert)
     % fonction de transfert comme argument
     % erpeeu : erreur en regime permanent entree echellon unite
-    % erperu : erreur en regime permanent entree rampe unite
+    % erperu : erreur en regime permanent entree rampe pente unite
     % deno : polynome denominateur
     % numo : polynome numerateur
     % T(s) = [Nn Nn-1 Nn-2 ...N2 N1 N0 ; Dn Dn-1 Dn-2 ...D2 D1 D0]
@@ -14,17 +14,17 @@ function[erpeeu, erperu] = performance(fct_transfert)
     N0 = numo(end);
     
 
-    if D0 ~= 0
+    if (D0 ~= 0)
         kp = N0/D0;
         erpeeu = 1 /(1+kp);
         erperu = Inf;
 
-    elseif (D0 ==0) && (D1 ~= 0)
+    elseif ((D0 ==0) && (D1 ~= 0))
         kv = N0/D1;
         erpeeu = 0;
         erperu = 1/kv;
 
-    elseif (D0==0) && (D1==0)
+    elseif ((D0==0) && (D1==0))
         erpeeu = 0;
         erperu = 0;
     end
