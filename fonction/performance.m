13ab386fec422fbc35a38da0f214115fc6e90eb7
Sammbayo2708 <samuelmbayo2708@gmail.com>
2024-07-11 03:07:21 +0100

    reponse indicielle

--- fonction/performance.m
+++ fonction/performance.m
@@ -1,32 +1,57 @@
-function[erpeeu, erperu] = performance(fct_transfert)
+function[erpeeu, erperu, reponseInd, psi] = performance(fct_transfert)
+    %syms t
     % fonction de transfert comme argument
     % erpeeu : erreur en regime permanent entree echellon unite
     % erperu : erreur en regime permanent entree rampe pente unite
     % deno : polynome denominateur
     % numo : polynome numerateur
     % T(s) = [Nn Nn-1 Nn-2 ...N2 N1 N0 ; Dn Dn-1 Dn-2 ...D2 D1 D0]
+    % reponseInd : fonction de t
 
-    nbr_coeff = length(fct_transfert)
-    deno = fct_transfert(2,1:nbr_coeff)
+    nbr_coeff = length(fct_transfert);
+    deno = fct_transfert(2,1:nbr_coeff);
     numo = fct_transfert(1,1:nbr_coeff);
     D0= deno(end);
     D1 = deno(end-1);
     N0 = numo(end);
-    
+    type = 0;
 
     if (D0 ~= 0)
+
         kp = N0/D0;
         erpeeu = 1 /(1+kp);
         erperu = Inf;
-
     elseif ((D0 ==0) && (D1 ~= 0))
         kv = N0/D1;
         erpeeu = 0;
         erperu = 1/kv;
-
+        type = 1;
     elseif ((D0==0) && (D1==0))
         erpeeu = 0;
         erperu = 0;
+        type = 2;
+    end
+
+    [reponseInd, psi] = reponse(fct_transfert, type);
+end
+
+function[reponseInd, psi] = reponse(fct_transfert, type)
+    psi = Inf;
+    syms t
+    fct_transfert = fct_transfert./fct_transfert(2,1);
+    if type == 0
+        if length(fct_transfert) == 2
+            y = fct_transfert(1,2)/fct_transfert(2, 2) * (1 - exp(-fct_transfert(2, 2)*t));
+            reponseInd = y;
+        elseif length(fct_transfert) == 3
+            gain = fct_transfert(1,3)/fct_transfert(2,end)
+            fct_transfert(1, end) = fct_transfert(2,end);
+            omega = sqrt(fct_transfert(1, end));
+            psi = fct_transfert(2,end-1)/(omega * 2);
+            theta = atan(psi/sqrt(1 - psi^2));
+            y = gain*(1 - (exp(-psi*omega*t)/sqrt(1 - psi^2)) * sin(sqrt(1 - psi^2)*omega*t + theta));
+            reponseInd = y;
+        end
     end
 end
 
--- fonction/runperf.m
+++ fonction/runperf.m
@@ -5,12 +5,50 @@ function runperf(fct_transfert)
     [stable, droite, gauche] = routh(fct_transfert(2,:));
     
     if stable == 0
-        disp('\nLe système est instable selon le critère de Routh');
+        disp('Le système est instable selon le critère de Routh');
         fprintf('\nIl y a %g poles dans le demi-plan de gauche et %d dans le demi-plan de de droite\n', droite, gauche);
     else
         disp('Le sytème est stable selon le critère de Routh')
         disp('')
         disp('Nous pouvons passer à la prochaine étape')
+        [erpeeu, erperu, reponseInd, psi] = performance(fct_transfert);
+        reponseInd = simplify(reponseInd);
+        psi
+        fprintf("\nL'erreur en regime permanent pour une entrée échelon est : %e", erpeeu);
+        fprintf("\nL'erreur en regime permanent pour une entrée rampe est : %e", erperu);
+        disp('La reponse indicielle est :');
+        disp(['y(t) = ', char(reponseInd)]);  % Convertir l'expression en chaîne de caractères
+
+        if psi ~= Inf
+            if psi == 0
+                courbe = ' NON AMORTIE';
+            elseif psi > 0 && psi < 1
+                courbe = ' SOUS-AMORTIE';
+            elseif psi == 1 
+                courbe = ' CRITIQUE AMORTIE';
+            elseif psi > 1
+                courbe = ' SUR-AMORTIE';
+            end
+            titre = strcat('Réponse indicielle de nature : ', courbe);
+            hold on
+            fplot(reponseInd)
+            title(titre)
+            grid on
+            xlabel('temps t')
+            ylabel('y(t)')
+            axis([0 10 0 2])
+            hold off
+        else
+            hold on
+            fplot(reponseInd)
+            title 'Allure de la reponse indicielle'
+            grid on
+            xlabel('temps t')
+            ylabel('y(t)')
+            hold off
+        end
+
+         
     end
 
 
